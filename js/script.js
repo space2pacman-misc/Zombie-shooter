1d072d6cda4fb15eeed518aa0e392220fc355e55
space2pacman <space2pacman@gmail.com>
2019-04-10 16:52:54 +0300

    mare rep

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@
 		}
 	}
 
-	addBot(5);
+	addBot(50);
 
 	map.addEventListener("click", function(e) {
 		player.pos.x = e.layerX - 25;
@@ -127,40 +127,12 @@ function lineBot(x0, y0, x1, y1,index){
   var dy0 = player.el.offsetTop - bots[index].el.offsetTop;
   var distance = Math.sqrt(dx0 * dx0 + dy0 * dy0);
 
-
-
-/*
-				for(var i= 0;i < quantityBot; i++) {
-
-
-
-
-					if (index == i) continue
-	var dxBot = bots[index].el.offsetLeft - bots[i].el.offsetLeft;
-  var dyBot = bots[index].el.offsetTop - bots[i].el.offsetTop;
-  var distanceBot = Math.sqrt(dxBot * dxBot + dyBot * dyBot);
-					if(distanceBot < bots[index].el.offsetWidth / 2 + bots[i].el.offsetWidth / 2) {
-						bots[index].el.style.background = "red";
-
-					if(bots[index].el.classList.contains("red")) {
-						break;
-					}
-					
-					//return false;
-				} else {
-					bots[index].el.style.background = "#0f0";
-				}
-				}*/
-
- 
-
-
 				if(distance < player.el.offsetWidth / 2 + bots[index].el.offsetWidth / 2) {
-					bots[index].el.style.background = "red";
-					bots[index].el.classList.add("red");
+					//bots[index].el.style.background = "red";
+					//bots[index].el.classList.add("red");
 					return false;
 				} else {
-					bots[index].el.style.background = "#0f0";
+					//bots[index].el.style.background = "#0f0";
 				}
 				
 			    if ((x0==x1) && (y0==y1)) return;
@@ -180,78 +152,3 @@ function lineBot(x0, y0, x1, y1,index){
 		    },50)
 		   
 		}
-
-
-
-
-/*
-function lineBot(x0, y0, x1, y1,index){
-			var dx = Math.abs(x1-x0);
-		    var dy = Math.abs(y1-y0);
-		    var sx = (x0 < x1) ? 1 : -1;
-		    var sy = (y0 < y1) ? 1 : -1;
-		    var err = dx-dy;
-
-		    i = setInterval(function() {
-		    	bots[index].el.style.left = x0  + "px";
-				bots[index].el.style.top = y0 + "px";
-
-				var boxCenter=[bots[index].el.offsetLeft+bots[index].el.offsetWidth/2, bots[index].el.offsetTop+bots[index].el.offsetWidth/2];
-				var angle = Math.atan2(player.pos.x - boxCenter[0], - (player.pos.y - boxCenter[1]) )*(180/Math.PI);
-
-				angle = Math.floor(angle);
-				bots[index].el.style.transform = "rotateZ(" + (270 + angle) + "deg)";
-
-				for(var i= 0;i < quantityBot; i++) {
-					if (index == i) continue
-					if(bots[index].el.offsetLeft + bots[index].el.offsetWidth >= bots[i].el.offsetLeft
-					&& bots[index].el.offsetLeft <= bots[i].el.offsetLeft + bots[i].el.offsetWidth
-					&& bots[index].el.offsetTop + bots[index].el.offsetHeight >= bots[i].el.offsetTop
-					&& bots[index].el.offsetTop <= bots[i].el.offsetTop + bots[i].el.offsetHeight) {
-					bots[index].el.style.background = "red";
-
-					if(bots[index].el.classList.contains("red")) {
-						break;
-					}
-
-					if(bots[index].el.offsetLeft + bots[index].el.offsetWidth == bots[i].el.offsetLeft) x0-=1;
-					if(bots[index].el.offsetLeft == bots[i].el.offsetLeft + bots[i].el.offsetWidth) x0+=1;
-					if(bots[index].el.offsetTop + bots[index].el.offsetHeight == bots[i].el.offsetTop) y0-=1; 
-					if(bots[index].el.offsetTop == bots[i].el.offsetTop + bots[i].el.offsetHeight) y0 += 1; 
-					
-					//return false;
-				} else {
-					bots[index].el.style.background = "#0f0";
-				}
-				}
-
-				if(bots[index].el.offsetLeft + bots[index].el.offsetWidth >= player.el.offsetLeft
-					&& bots[index].el.offsetLeft <= player.el.offsetLeft + player.el.offsetWidth
-					&& bots[index].el.offsetTop + bots[index].el.offsetHeight >= player.el.offsetTop
-					&& bots[index].el.offsetTop <= player.el.offsetTop + player.el.offsetHeight) {
-					bots[index].el.style.background = "red";
-					bots[index].el.classList.add("red");
-					return false;
-				} else {
-					bots[index].el.style.background = "#0f0";
-				}
-				
-			    if ((x0==x1) && (y0==y1)) return;
-			    var e2 = 2*err;
-			    if (e2 >-dy){ 
-			    	err -= dy; 
-			    	x0  += sx; 
-			    }
-			    if (e2 < dx){ 
-			    	err += dx; 
-			    	y0  += sy; 
-			    }
-			    if (x0 == player.pos.x && y0 == player.pos.y) {
-			    	//clearInterval(i);
-			    	startMove = true;
-			    }
-		    },50)
-		   
-		}
-
-*/
